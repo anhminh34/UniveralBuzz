90d1b4cf182d7e2fad5c27fea47509cf9a374547
Minh Nguyen <anhminh34@gmail.com>
2024-04-23 16:44:02 -0700

    current progress for user information to database

--- universalbuzzSpring/src/main/java/com/sacstate/universalbuzz/service/LoginService.java
+++ universalbuzzSpring/src/main/java/com/sacstate/universalbuzz/service/LoginService.java
@@ -9,4 +9,18 @@ public class LoginService {
      public Page<UserInfo> getUsers(int page, int size) {
          return userRepo.findall(PageRequest.of(page,size, Sort.by("name")));
      }
+
+     public UserInfo getUser(String id){
+         return userRepo.findById(id).orElseThrow(()->new RuntimeException("Contact not found"));
+     }
+
+     public UserInfo createNewUser(Userinfo newUser){
+         return userRepo.save(newUser);
+     }
+
+     public void deleteUser(UserInfo user){
+         // assignment
+     }
+     
+     public String uploadPhoto(String id, MulipartFile file)
 }
