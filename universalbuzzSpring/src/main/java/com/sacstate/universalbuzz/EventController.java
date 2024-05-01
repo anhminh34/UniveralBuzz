d495c4c21f933171ef73510be146b49e0680eb8b
bomrr <cparsons2@csus.edu>
2024-05-01 14:45:29 -0700

    Current (still not working) progress on Events pages

--- universalbuzzSpring/src/main/java/com/sacstate/universalbuzz/EventController.java
+++ universalbuzzSpring/src/main/java/com/sacstate/universalbuzz/EventController.java
@@ -2,23 +2,14 @@ package com.sacstate.universalbuzz;
 //EventController.java
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
 import jakarta.servlet.http.HttpSession;
@@ -36,69 +27,8 @@ public class EventController {
         this.eventRepository = eventRepository;
     }
 
-    /*@GetMapping("/")
-    public String root() {
-        return "Events";
-    }*/
-
-    /*@GetMapping("/api/Events")
-	public ResponseEntity<List<Event>> getAllEvents(@RequestParam(required = false) String title) {
-        try {
-            List<Event> events = new ArrayList<Event>();
-
-            if (title == null) {
-                eventRepository.findAll().forEach(events::add);
-            }else {
-				eventRepository.findByeventNameContaining(title).forEach(events::add);
-            }
-
-            if (events.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(events, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }*/
-
-	/*@GetMapping("/Events/{id}")
-	public ResponseEntity<Event> getEventById(@PathVariable("id") long id) {
-		Optional<Event> eventData = eventRepository.findByID(id);
-	}*/
-
-	/*@PostMapping("/Events")
-	public ResponseEntity<Event> createEvent(@RequestBody Event event) {
-		try {
-			Event _event = eventRepository.save(new Event(event.getEventName(), event.getEventDate(), event.getEventLocation(), event.getEventDescription()));
-			return new ResponseEntity<>(_event, HttpStatus.CREATED);
-		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}*/
-
-	/*@PutMapping("/Events/{id}")
-	public ResponseEntity<Event> updateEvent(@PathVariable("id") long id, @RequestBody Event Event) {
-		
-	}*/
-
-	/*@DeleteMapping("/Events/{id}")
-	public ResponseEntity<HttpStatus> deleteEvent(@PathVariable("id") long id) {
-		
-	}*/
-
-	/*@DeleteMapping("/Events")
-	public ResponseEntity<HttpStatus> deleteAllEvents() {
-		
-	}*/
-
-	/*@GetMapping("/Events/published")
-	public ResponseEntity<List<Event>> findByPublished() {
-		
-	}*/
-
     // When an event is submitted, save it, and reload the page.
-    @PostMapping("/submitEvent")
+    @GetMapping("/submitEvent")
     public RedirectView submitEventForm(@ModelAttribute Event event, Model model) {
         try {
             // Save the event
@@ -108,13 +38,11 @@ public class EventController {
             System.out.println(savedEvent.toString());
 
             // Redirect to the EventsPage after submitting the event
-            return new RedirectView("/EventsPage");
+            return new RedirectView("/EventSubmissionPage");
 
         } catch (Exception e) {
-
-            e.printStackTrace();
-            
             // Redirect to an error page if an exception occurs
+            e.printStackTrace();
             return new RedirectView("/error");
         }
     }
@@ -135,7 +63,7 @@ public class EventController {
             System.out.println("Running eventsPage controller code.");
             eventService.printAllEvents();
 
-            return "Events";
+            return "EventsPage.html";
         
         } catch (Exception e) {
             System.out.println("eventsPage directory error");
